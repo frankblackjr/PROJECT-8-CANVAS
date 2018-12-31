6e0a17e4858d5bc3f201cd48e50d8fb0b9e0e728
frankblackjr <35541653+frankblackjr@users.noreply.github.com>
2018-12-30 22:09:01 -0500

    And so it begins

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext("2d");
+canvas.fillStyle = "#335454";
+ctx.fillRect(30, 100, 25, 300);
+
+
+var timer = setInterval(changer, 10);
+
+function changer() {
+
+}
