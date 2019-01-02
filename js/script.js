22a263cef251ec04426ca82acf9bf4c2063c0442
frankblackjr <35541653+frankblackjr@users.noreply.github.com>
2019-01-01 20:12:16 -0500

    Drawing on canvas with tut link

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
+/*
+Canvas tut https://www.youtube.com/watch?v=EO6OkltgudE
+*/
 var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var ctx = canvas.getContext('2d');
+var c = canvas.getContext('2d');
+//Rectangles
+//context.fillRect(x, y, w, h);
+c.fillRect(100, 100, 100, 100);
+c.fillRect(300, 100, 100, 100);
+c.fillRect(500, 100, 100, 100);
 
-ctx.fillRect(100, 100, 100, 100);
-ctx.fillRect(300, 100, 100, 100);
-ctx.fillRect(500, 100, 100, 100);
-ctx.fillRect(100, 300, 100, 100);
-ctx.fillRect(300, 300, 100, 100);
-ctx.fillRect(500, 300, 100, 100);
+//Lines
+c.beginPath();
+//moveTo(x, y) Starting point.
+c.moveTo(50, 300);
